89d3cc272f9ecd488ace7374d62a2407fdd60d5f
오소영 <Soyoung@osoyeong-ui-MacBook-Air.local>
2017-04-28 23:51:51 +0900

    imageview 독학 공부 170321

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnResize: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imgOn = UIImage(named: "lamp_on.gif")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
 
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            btnResize.setTitle("축소", for: .normal)
+
+        }
+        
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+    
 }
 
